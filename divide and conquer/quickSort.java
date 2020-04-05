dc23ba7651ae92fe1ac299097af52571d3186125
Abhi2080 <57823658+Abhi2080@users.noreply.github.com>
2020-04-06 02:07:10 +0530

    Update quickSort.java

--- divide and conquer/quickSort.java
+++ divide and conquer/quickSort.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 public class quickSort
 {
   static void swap(int a, int b)
@@ -37,12 +38,23 @@ public class quickSort
   }
   public static void main(String args[])
   {
-    int[] numbers = {12,45,67,8,9,34,56,87,90};
-
+    Random rd = new Random();
+    int[] numbers = new int[10000];
+    for(int i =0; i<numbers.length; i++)
+    {
+      numbers[i] = rd.nextInt();
+      System.out.println(numbers[i]);
+    }
+    System.out.println();
     int n = numbers.length;
+    long start = System.currentTimeMillis();
     quickSort(numbers,0,(n-1));
+    long end = System.currentTimeMillis();
     for(int i = 0; i<n; i++)
     {
     System.out.println(numbers[i]);}
+    System.out.println("Total execution time in millis: " + (end - start));
+
   }
+
 }
